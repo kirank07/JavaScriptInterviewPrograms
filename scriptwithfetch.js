90878b7c698967bd909ff2b4c2f5d7aeb7215395
Kiran Khade <53072447+kirank07@users.noreply.github.com>
2023-09-16 12:39:54 +0530

    Create scriptwithfetch.js

---
+++ scriptwithfetch.js
@@ -0,0 +1,14 @@
+
+let getSpacePeople = () => 
+fetch("http://api.open-notify.org/astros.json")
+.then((res) => res.json());
+
+getSpacePeople().then(console.log);
+
+let spaceNames = () =>
+getSpacePeople()
+.then((json) => json.people)
+.then((people) => people.map((p) => p.name))
+.then((names) => names.join(", "));
+
+spaceNames().then(console.log);
